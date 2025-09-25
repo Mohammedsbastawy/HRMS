e2eccb480c2dc5d186a1e986ad358a0b3cbb8391
Mohammed Bastawy <mohammedsbastawy@gmail.com>
2025-09-25 12:30:13 +0000

    PS D:\hrms> npm run dev
    > nextn@0.1.0 dev
    > next dev --turbopack -p 900

--- src/app/(dashboard)/employees/page.tsx
+++ src/app/(dashboard)/employees/page.tsx
@@ -1,8 +1,6 @@
 
 import { EmployeesPageClient } from './_components/employees-client';
 
-// This is now just a wrapper component. 
-// All logic is handled in the Client Component.
 export default function EmployeesDataPage() {
     return <EmployeesPageClient />;
 }
