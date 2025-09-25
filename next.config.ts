a27a42714518071dabacefa015964cc14e5da3b9
Mohammed Bastawy <mohammedsbastawy@gmail.com>
2025-09-25 12:35:20 +0000

    PS D:\hrms> npm run dev
    > nextn@0.1.0 dev
    > next dev --turbopack -p 900

--- next.config.ts
+++ next.config.ts
@@ -30,15 +30,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  devIndicators: {
-    buildActivity: false,
-  },
-  experimental: {
-    allowedDevOrigins: [
-      'https://*.cloudworkstations.dev',
-      'https://*.firebase.studio',
-    ],
-  },
 };
 
 export default nextConfig;
