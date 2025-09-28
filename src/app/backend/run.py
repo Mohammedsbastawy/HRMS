b24dff555599493fec9e3e7788938497fa8473a0
Mohammed Bastawy <mohammedsbastawy@gmail.com>
2025-09-28 09:40:52 +0000

    } Failed to proxy http://127.0.0.1:5000/api/job-titles Error: connect EC

--- src/app/backend/run.py
+++ src/app/backend/run.py
@@ -2,4 +2,5 @@ from app import app, init_db
 
 if __name__ == "__main__":
     init_db()
+    # Explicitly setting host to '0.0.0.0' ensures it's accessible from the Next.js proxy.
     app.run(host='0.0.0.0', port=5000, debug=True)
